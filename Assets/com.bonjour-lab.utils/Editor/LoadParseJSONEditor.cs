84023db019a777764ab43483fbf582fc7ba65df1
Alexandre Rivaux <alex@bonjour-lab.com>
2022-09-20 10:05:48 +0200

    Update JSON for build compilation

---
+++ Assets/com.bonjour-lab.utils/Editor/LoadParseJSONEditor.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Bonjour.JSON { 
+    [CustomEditor(typeof(Bonjour.JSON.LoadParseJSON))]
+    [CanEditMultipleObjects]
+    public class LoadParseJSONEditor : Editor{
+        SerializedProperty configfile;
+        SerializedProperty pathToFile;
+        SerializedProperty sourcetype;
+
+        void OnEnable()
+        {
+            configfile            = serializedObject.FindProperty ("configfile");
+            pathToFile            = serializedObject.FindProperty ("pathToFile");
+            sourcetype            = serializedObject.FindProperty ("sourcetype");
+        }
+
+         public override void OnInspectorGUI () {
+                // DrawDefaultInspector();
+                serializedObject.Update ();
+
+                EditorGUILayout.PropertyField(configfile, new GUIContent("JSON file", "Set the name of the json file without the extension"));
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(sourcetype, new GUIContent("Source path type", "Define if you want to taregt the Streaming Asset, a cutom path or a path inside the Assets folder"));
+            
+                LoadParseJSON.SOURCETYPE _type = (LoadParseJSON.SOURCETYPE) SerializedPropertyExtensions.GetValue(sourcetype);
+                if( _type == LoadParseJSON.SOURCETYPE.CUSTOM ||
+                    _type == LoadParseJSON.SOURCETYPE.FROM_ASSETS_ROOT){
+                        EditorGUILayout.PropertyField(pathToFile, new GUIContent("Path to File"));
+                }
+                serializedObject.ApplyModifiedProperties();
+         }
+    }
+}
--- Assets/com.bonjour-lab.utils/Runtime/JSON/LoadParseJSON.cs
+++ Assets/com.bonjour-lab.utils/Runtime/JSON/LoadParseJSON.cs
@@ -5,78 +5,93 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-public class LoadParseJSON : MonoBehaviour
+namespace Bonjour.JSON
 {
-    public string configfile;
-    public string pathToFile;
 
-    private JObject config;
-    private string configAsAString;
+    public class LoadParseJSON : MonoBehaviour
+    {
+        public string configfile;
+        public string pathToFile;
 
-    public enum SOURCETYPE{
-        STREAMING_ASSET,
-        RESOURCES_FOLDER,
-        FROM_ASSETS_ROOT,
-        CUSTOM
-    }
+        private JObject config;
+        private string configAsAString;
 
-    public SOURCETYPE sourcetype = SOURCETYPE.STREAMING_ASSET;
-
-    private void Awake(){
-        LoadJson();
-        Debug.Log(config);
-    }
-
-    public void LoadJson(){
-        config = null;
-        string sourcePath = GetSourcePath();
-        pathToFile = Path.Combine(sourcePath, $"{configfile}.json");
-        using (StreamReader stream = new StreamReader(pathToFile)) 
-	    {
-	        string json = stream.ReadToEnd();
-	        config = JObject.Parse(json);
-        }
-    }
-
-    public void LoadAsString(){
-        configAsAString = null;
-        string sourcePath = GetSourcePath();
-        pathToFile = Path.Combine(sourcePath, $"{configfile}.json");
-        using (StreamReader stream = new StreamReader(pathToFile)) 
-	    {
-	        configAsAString = stream.ReadToEnd();
-        }
-    }
-
-    public JObject GetConfig(){
-        if(config == null){
-            LoadJson();
-        }
-        return config;
-    }
-
-    public string GetConfigAsString(){
-        if(configAsAString == null){
-            LoadAsString();
-        }
-        return configAsAString;
-    }
-
-    private string GetSourcePath(){
-        switch(sourcetype){
-            default:
-            case SOURCETYPE.STREAMING_ASSET : return Application.streamingAssetsPath;
-            case SOURCETYPE.RESOURCES_FOLDER : return Application.dataPath + "/Resources";
-            case SOURCETYPE.FROM_ASSETS_ROOT : return Application.dataPath + "/" + pathToFile;
-            case SOURCETYPE.CUSTOM : return pathToFile;
-        }
-    }
-
-    public void SaveJSONConfig(JObject config){
-        using (StreamWriter file = File.CreateText(pathToFile))
-        using (JsonTextWriter writer = new JsonTextWriter(file))
+        public enum SOURCETYPE
         {
-            config.WriteTo(writer);
+            STREAMING_ASSET,
+            RESOURCES_FOLDER,
+            FROM_ASSETS_ROOT,
+            CUSTOM
+        }
+
+        public SOURCETYPE sourcetype = SOURCETYPE.STREAMING_ASSET;
+
+        private void Awake()
+        {
+            LoadJson();
+            Debug.Log(config);
+        }
+
+        public void LoadJson()
+        {
+            config = null;
+            string sourcePath = GetSourcePath();
+            pathToFile = Path.Combine(sourcePath, $"{configfile}.json");
+            using (StreamReader stream = new StreamReader(pathToFile))
+            {
+                string json = stream.ReadToEnd();
+                config = JObject.Parse(json);
+            }
+        }
+
+        public void LoadAsString()
+        {
+            configAsAString = null;
+            string sourcePath = GetSourcePath();
+            pathToFile = Path.Combine(sourcePath, $"{configfile}.json");
+            using (StreamReader stream = new StreamReader(pathToFile))
+            {
+                configAsAString = stream.ReadToEnd();
+            }
+        }
+
+        public JObject GetConfig()
+        {
+            if (config == null)
+            {
+                LoadJson();
+            }
+            return config;
+        }
+
+        public string GetConfigAsString()
+        {
+            if (configAsAString == null)
+            {
+                LoadAsString();
+            }
+            return configAsAString;
+        }
+
+        private string GetSourcePath()
+        {
+            switch (sourcetype)
+            {
+                default:
+                case SOURCETYPE.STREAMING_ASSET: return Application.streamingAssetsPath;
+                case SOURCETYPE.RESOURCES_FOLDER: return Application.dataPath + "/Resources";
+                case SOURCETYPE.FROM_ASSETS_ROOT: return Application.dataPath + "/" + pathToFile;
+                case SOURCETYPE.CUSTOM: return pathToFile;
+            }
+        }
+
+        public void SaveJSONConfig(JObject config)
+        {
+            using (StreamWriter file = File.CreateText(pathToFile))
+            using (JsonTextWriter writer = new JsonTextWriter(file))
+            {
+                config.WriteTo(writer);
+            }
         }
     }
 }
